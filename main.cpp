64a794f39eae925c209433e681e2f584bb7fa088
mariuszmaximus <mariusz.maximus@gmail.com>
2022-07-11 22:53:26 +0200

    sample based on https://www.bogotobogo.com/Qt/Qt5_QTcpServer_Multithreaded_Client_Server.php

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// main.cpp
+
+#include <QCoreApplication>
+#include "myserver.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    // Make a server and starts it
+    MyServer server;
+    server.startServer();
+
+    return a.exec();
+}
